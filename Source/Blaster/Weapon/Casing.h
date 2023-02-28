17497dc5e3fa92aca8225a89f8eeb66167caad2b
‘zihua-d’ <1393110779@qq.com>
2023-02-28 19:51:28 +0800

    弹壳物理

--- Source/Blaster/Weapon/Casing.h
+++ Source/Blaster/Weapon/Casing.h
@@ -12,12 +12,22 @@ class BLASTER_API ACasing : public AActor
 	
 public:	
 	ACasing();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	
 private:
 	UPROPERTY(VisibleAnywhere)
 	UStaticMeshComponent* CasingMesh;
-protected:
-	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere)
+	float ShellEjectionImpulse;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* ShellSound;
 public:	
 
 };
