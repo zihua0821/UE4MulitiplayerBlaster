d63da3aa2a75474c7eff8187cabb03eb6ba08e91
‘zihua-d’ <1393110779@qq.com>
2023-03-30 03:12:54 +0800

    返回主菜单

--- Source/Blaster/Blaster.Build.cs
+++ Source/Blaster/Blaster.Build.cs
@@ -10,7 +10,7 @@ public class Blaster : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Niagara" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "MultiplayerSessions", "OnlineSubsystem", "OnlineSubsystemSteam" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
