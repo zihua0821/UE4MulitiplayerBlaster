082686638846c036c669d4ae7e7982a1fe16f5a2
‘zihua-d’ <1393110779@qq.com>
2023-04-02 03:14:30 +0800

    团队得分结算

--- Source/Blaster/PlayerController/BlasterPlayerController.h
+++ Source/Blaster/PlayerController/BlasterPlayerController.h
@@ -82,6 +82,9 @@ protected:
 
 	UFUNCTION()
 	void OnRep_ShowTeamScores();
+
+	FString GetInfoText(const TArray<class ABlasterPlayerState*>& Players);
+	FString GetTeamsInfoText(class ABlasterGameState* BlasterGameState);
 private:
 	UPROPERTY()
 	class ABlasterHUD* BlasterHUD;
