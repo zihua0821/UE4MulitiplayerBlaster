94df17d284593f13b83c74b1a64ca8f4594c4fca
‘zihua-d’ <1393110779@qq.com>
2023-04-21 21:50:12 +0800

    代码整理-游戏状态

--- Source/Blaster/GameState/BlasterGameState.cpp
+++ Source/Blaster/GameState/BlasterGameState.cpp
@@ -1,8 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "BlasterGameState.h"
-
 #include "Blaster/PlayerState/BlasterPlayerState.h"
 #include "Net/UnrealNetwork.h"
 #include "Blaster/PlayerController/BlasterPlayerController.h"
@@ -11,6 +7,7 @@ void ABlasterGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& Ou
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
+	//注册复制变量
 	DOREPLIFETIME(ABlasterGameState, TopScoringPlayers);
 	DOREPLIFETIME(ABlasterGameState, RedTeamScore);
 	DOREPLIFETIME(ABlasterGameState, BlueTeamScore);
@@ -18,16 +15,16 @@ void ABlasterGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& Ou
 
 void ABlasterGameState::UpdateTopScore(ABlasterPlayerState* ScoringPlayer)
 {
-	if (TopScoringPlayers.Num() == 0)
+	if (TopScoringPlayers.Num() == 0)//如果没有最高分玩家则将该玩家添加到数组中，并且更新最高分
 	{
 		TopScoringPlayers.Add(ScoringPlayer);
 		TopScore = ScoringPlayer->GetScore();
 	}
-	else if (ScoringPlayer->GetScore() == TopScore)
+	else if (ScoringPlayer->GetScore() == TopScore)//如果与最高分相同则将该玩家添加到数组中
 	{
 		TopScoringPlayers.AddUnique(ScoringPlayer);
 	}
-	else if (ScoringPlayer->GetScore() > TopScore)
+	else if (ScoringPlayer->GetScore() > TopScore)//如果大于最高分，则清空最高分玩家数组，将当前玩家添加的数组中
 	{
 		TopScoringPlayers.Empty();
 		TopScoringPlayers.AddUnique(ScoringPlayer);
@@ -37,7 +34,9 @@ void ABlasterGameState::UpdateTopScore(ABlasterPlayerState* ScoringPlayer)
 
 void ABlasterGameState::RedTeamScores()
 {
+	//在服务器上为红队加分
 	++RedTeamScore;
+	//服务器玩家更新HUD
 	ABlasterPlayerController* BPlayer = Cast<ABlasterPlayerController>(GetWorld()->GetFirstPlayerController());
 	if (BPlayer)
 	{
@@ -47,7 +46,9 @@ void ABlasterGameState::RedTeamScores()
 
 void ABlasterGameState::BlueTeamScores()
 {
+	//在服务器上为蓝队加分
 	++BlueTeamScore;
+	//服务器玩家更新HUD
 	ABlasterPlayerController* BPlayer = Cast<ABlasterPlayerController>(GetWorld()->GetFirstPlayerController());
 	if (BPlayer)
 	{
@@ -57,6 +58,7 @@ void ABlasterGameState::BlueTeamScores()
 
 void ABlasterGameState::OnRep_RedTeamScore()
 {
+	//客户端玩家更新HUD
 	ABlasterPlayerController* BPlayer = Cast<ABlasterPlayerController>(GetWorld()->GetFirstPlayerController());
 	if (BPlayer)
 	{
@@ -66,6 +68,7 @@ void ABlasterGameState::OnRep_RedTeamScore()
 
 void ABlasterGameState::OnRep_BlueTeamScore()
 {
+	//客户端玩家更新HUD
 	ABlasterPlayerController* BPlayer = Cast<ABlasterPlayerController>(GetWorld()->GetFirstPlayerController());
 	if (BPlayer)
 	{
--- Source/Blaster/GameState/BlasterGameState.h
+++ Source/Blaster/GameState/BlasterGameState.h
@@ -1,42 +1,71 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameState.h"
 #include "BlasterGameState.generated.h"
+
 class ABlasterPlayerState;
+
 /**
- * 
+ * 游戏状态
+ * 继承自GameState 拥有基础的多人游戏配置
+ * 储存队伍信息 队伍玩家 队伍得分
+ * 控制队伍分数更新
  */
 UCLASS()
 class BLASTER_API ABlasterGameState : public AGameState
 {
 	GENERATED_BODY()
 public:
+	//注册复制变量
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	/**
+	 * 更新得分最高玩家
+	 * @param ScoringPlayer 有分数更新的玩家状态
+	 */
 	void UpdateTopScore(ABlasterPlayerState* ScoringPlayer);
-	
+
+	//分数最高玩家状态指针数组 复制变量
 	UPROPERTY(Replicated)
 	TArray<ABlasterPlayerState*> TopScoringPlayers;
 
+	/**
+	 * 为红队添加一分 仅服务器调用
+	 */
 	void RedTeamScores();
+
+	/**
+	 * 为蓝队添加一分 仅服务器调用
+	 */
 	void BlueTeamScores();
-	
+
+	//红队玩家状态指针数组
 	TArray<ABlasterPlayerState*> RedTeam;
+
+	//蓝队玩家状态指针数组
 	TArray<ABlasterPlayerState*> BlueTeam;
 
+	//红队得分 复制变量 变量更新时触发回调
 	UPROPERTY(ReplicatedUsing = OnRep_RedTeamScore)
 	float RedTeamScore = 0.f;
-
+	
+	//蓝队得分 复制变量 变量更新时触发回调
 	UPROPERTY(ReplicatedUsing = OnRep_BlueTeamScore)
 	float BlueTeamScore = 0.f;
 
+	/**
+	 * 红队得分复制更新时触发的回调 仅客户端触发
+	 */
 	UFUNCTION()
 	void OnRep_RedTeamScore();
 
+	/**
+	 * 蓝队得分复制更新时触发的回调 仅客户端触发
+	 */
 	UFUNCTION()
 	void OnRep_BlueTeamScore();
 private:
+	//最高得分
 	float TopScore = 0.f;
 };
