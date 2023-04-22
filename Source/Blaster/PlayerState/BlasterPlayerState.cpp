8d0fc23cbbff4d684e68ee96b5f34c2b347ebb80
‘zihua-d’ <1393110779@qq.com>
2023-04-22 09:31:36 +0800

    代码整理-玩家状态

--- Source/Blaster/PlayerState/BlasterPlayerState.cpp
+++ Source/Blaster/PlayerState/BlasterPlayerState.cpp
@@ -1,8 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "BlasterPlayerState.h"
-
 #include "Blaster/Character/BlasterCharacter.h"
 #include "Blaster/PlayerController/BlasterPlayerController.h"
 #include "Net/UnrealNetwork.h"
@@ -11,6 +7,7 @@ void ABlasterPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& 
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
+	//注册复制变量
 	DOREPLIFETIME(ABlasterPlayerState, Defeats);
 	DOREPLIFETIME(ABlasterPlayerState, Team);
 }
@@ -19,6 +16,7 @@ void ABlasterPlayerState::OnRep_Score()
 {
 	Super::OnRep_Score();
 
+	//得分变化时更新玩家HUD中的得分
 	Character = Character == nullptr ? Cast<ABlasterCharacter>(GetPawn()) : Character;
 	if (Character)
 	{
@@ -32,6 +30,7 @@ void ABlasterPlayerState::OnRep_Score()
 
 void ABlasterPlayerState::OnRep_Defeats()
 {
+	//被击败数变化时更新玩家HUD中的得分
 	Character = Character == nullptr ? Cast<ABlasterCharacter>(GetPawn()) : Character;
 	if (Character)
 	{
@@ -45,6 +44,7 @@ void ABlasterPlayerState::OnRep_Defeats()
 
 void ABlasterPlayerState::AddToScore(float ScoreAmount)
 {
+	//仅服务器调用 为服务器权威版本的玩家状态添加分数
 	SetScore(GetScore() + ScoreAmount);
 	Character = Character == nullptr ? Cast<ABlasterCharacter>(GetPawn()) : Character;
 	if (Character)
@@ -59,6 +59,7 @@ void ABlasterPlayerState::AddToScore(float ScoreAmount)
 
 void ABlasterPlayerState::AddToDefeats(int32 DefeatsAmount)
 {
+	//仅服务器调用 为服务器权威版本的玩家状态添加被击败数
 	Defeats += DefeatsAmount;
 	Character = Character == nullptr ? Cast<ABlasterCharacter>(GetPawn()) : Character;
 	if (Character)
@@ -73,6 +74,7 @@ void ABlasterPlayerState::AddToDefeats(int32 DefeatsAmount)
 
 void ABlasterPlayerState::OnRep_Team()
 {
+	//玩家队伍变化时更新玩家角色的颜色
 	ABlasterCharacter* BCharacter = Cast <ABlasterCharacter>(GetPawn());
 	if (BCharacter)
 	{
@@ -82,8 +84,8 @@ void ABlasterPlayerState::OnRep_Team()
 
 void ABlasterPlayerState::SetTeam(ETeam TeamToSet)
 {
+	//仅服务器调用 为服务器权威版本的玩家状态设置队伍
 	Team = TeamToSet;
-
 	ABlasterCharacter* BCharacter = Cast <ABlasterCharacter>(GetPawn());
 	if (BCharacter)
 	{
--- Source/Blaster/PlayerState/BlasterPlayerState.h
+++ Source/Blaster/PlayerState/BlasterPlayerState.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -8,34 +6,61 @@
 #include "BlasterPlayerState.generated.h"
 
 /**
- * 
+ * 玩家状态
+ * 继承自PlayerState
+ * 内置玩家得分变量
+ * 储存玩家信息 得分 被击败数 玩家队伍
  */
 UCLASS()
 class BLASTER_API ABlasterPlayerState : public APlayerState
 {
 	GENERATED_BODY()
 public:
+
+	//注册复制变量
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	//覆写得分复制更新回调
 	virtual void OnRep_Score() override;
-	
+
+	/**
+	 * 被淘汰数复制更新回调
+	 */
 	UFUNCTION()
 	virtual void OnRep_Defeats();
-	
+
+	/**
+	 * 为玩家添加分数
+	 * @param ScoreAmount 添加的分数 
+	 */
 	void AddToScore(float ScoreAmount);
+	
+	/**
+	 * 为玩家添加被淘汰数
+	 * @param DefeatsAmount 添加的被淘汰数 
+	 */
 	void AddToDefeats(int32 DefeatsAmount);
+	
 private:
+
+	//玩家角色指针
 	UPROPERTY()
 	class ABlasterCharacter* Character;
-	
+
+	//玩家控制器指针
 	UPROPERTY()
 	class ABlasterPlayerController* Controller;
 
+	//玩家被淘汰数 复制变量 复制后触发回调
 	UPROPERTY(ReplicatedUsing = OnRep_Defeats)
 	int32 Defeats;
 
+	//玩家队伍 复制变量 复制后触发回调
 	UPROPERTY(ReplicatedUsing = OnRep_Team)
 	ETeam Team = ETeam::ET_NoTeam;
 
+	/**
+	 * 玩家队伍复制更新回调
+	 */
 	UFUNCTION()
 	void OnRep_Team();
 public:
